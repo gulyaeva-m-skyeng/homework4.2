8f69f514b2bbadf8c300635e70b94080faef6a7e
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-24 15:59:59 +0300

    дз 4.2

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+//task 1
+        System.out.println("Task 1");
+        int desiredSum = 2_459_000;
+        int sum = 0;
+        int amount = 15_000;
+        double percent = 1D / 100;
+        int month = 0;
+        while (sum < desiredSum) {
+            sum += amount;
+            sum = (int) (sum * (1 + percent));
+            month++;
+            System.out.println("Месяц "+month+ ", сумма накоплений равна " + sum + " рублей");
+
+        }
+    }
+}
